ee3321662a90782961d4305c7f6cd2acdfa5c0cd
“shanghaijunheng” <78899500@qq.com>
2017-01-22 09:04:15 +0800

    Initial Commit

---
+++ 给图片添加水印/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  给图片添加水印
+//
+//  Created by 上海均衡 on 2017/1/22.
+//  Copyright © 2017年 上海均衡. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
